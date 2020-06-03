ecb5d69e5e9e14b1be93007659eda0f79823a63d
Sumit Semwal <sumit.semwal@linaro.org>
2020-06-03 16:03:17 +0530

    fts: debug logs
    Signed-off-by: Sumit Semwal <sumit.semwal@linaro.org>
    Change-Id: Ie700558fd3a0148483a0ee99ac38ffcd87b80471

--- drivers/input/touchscreen/stm/fts.c
+++ drivers/input/touchscreen/stm/fts.c
@@ -1474,6 +1474,7 @@ static void touchsim_refresh_coordinates(struct fts_touchsim *touchsim)
 static void touchsim_report_contact_event(struct input_dev *dev, int slot_id,
 						int x, int y, int z)
 {
+printk("SS: %s, slot_id: %d, x: %d, y: %d, z: %d\n", __func__,slot_id, x, y, z);
 	/* report the cordinates to the input subsystem */
 	input_mt_slot(dev, slot_id);
 	input_report_key(dev, BTN_TOUCH, true);
@@ -2470,7 +2471,7 @@ static bool fts_enter_pointer_event_handler(struct fts_ts_info *info, unsigned
 	unsigned int touch_condition = 1, tool = MT_TOOL_FINGER;
 	int x, y, z, major, minor, distance;
 	u8 touchType;
-
+printk("SS: %s, event: %c\n", __func__, *event);
 	if (!info->resume_bit)
 		goto no_report;
 
@@ -2547,8 +2548,8 @@ static bool fts_enter_pointer_event_handler(struct fts_ts_info *info, unsigned
 	input_report_key(info->input_dev, BTN_TOUCH, touch_condition);
 	input_mt_report_slot_state(info->input_dev, tool, 1);
 
-	/* pr_info("%s : TouchID = %d,Touchcount = %d\n", __func__,
-	  *	touchId,touchcount); */
+printk("%s : TouchID = %d,Touchcondition = %d\n", __func__,
+	 touchId,touch_condition);
 
 	input_report_abs(info->input_dev, ABS_MT_POSITION_X, x);
 	input_report_abs(info->input_dev, ABS_MT_POSITION_Y, y);
@@ -3186,6 +3187,7 @@ static irqreturn_t fts_interrupt_handler(int irq, void *handle)
 	const unsigned char EVENTS_REMAINING_MASK = 0x1F;
 	unsigned char events_remaining = 0;
 	unsigned char *evt_data;
+printk("SS: %s\n", __func__);
 
 	/* It is possible that interrupts were disabled while the handler is
 	 * executing, before acquiring the mutex. If so, simply return.
