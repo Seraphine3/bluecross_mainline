rmi1974/git.linaro.org-john.stultz-android-dev

drm/msm/dsi: configure dsc registers
DSC configuration registers need to be programmed in video mode and
command mode, add support for that

Signed-off-by: Vinod Koul <vkoul@kernel.org>

drivers/gpu/drm/msm/dsi/dsi.xml.h
@@ -1726,5 +1726,13 @@ static inline uint32_t REG_DSI_10nm_PHY_LN_TX_DCTRL(uint32_t i0) { return 0x0000
 
 #define REG_DSI_10nm_PHY_PLL_COMMON_STATUS_ONE			0x000001a0
 
+#define REG_DSI_VIDEO_COMPRESSION_MODE_CTRL			0x000002a0
 
+#define REG_DSI_VIDEO_COMPRESSION_MODE_CTRL2			0x000002a4
+
+#define REG_DSI_COMMAND_COMPRESSION_MODE_CTRL			0x000002a8
+
+#define REG_DSI_COMMAND_COMPRESSION_MODE_CTRL2			0x000002ac
+
+#define REG_DSI_COMMAND_COMPRESSION_MODE_CTRL3			0x000002b0
 #endif /* DSI_XML */

Seraphine3/bluecross_mainline

drivers/gpu/drm/msm/dsi/dsi.xml.h
@@ -2310,4 +2310,14 @@ static inline uint32_t REG_DSI_7nm_PHY_LN_TX_DCTRL(uint32_t i0) { return 0x00000
 
 #define REG_DSI_7nm_PHY_PLL_PERF_OPTIMIZE			0x00000260
 
+#define REG_DSI_VIDEO_COMPRESSION_MODE_CTRL			0x0000029c
+
+#define REG_DSI_VIDEO_COMPRESSION_MODE_CTRL2			0x000002a0
+
+#define REG_DSI_COMMAND_COMPRESSION_MODE_CTRL			0x000002a4
+
+#define REG_DSI_COMMAND_COMPRESSION_MODE_CTRL2			0x000002a8
+
+#define REG_DSI_COMMAND_COMPRESSION_MODE_CTRL3			0x000002ac
+
 #endif /* DSI_XML */